0c47020a9c98b975c4867d02f1ae94ce61572bca
Siva Anand <sivakkdi2000@gmail.com>
2022-06-27 11:52:29 -0500

    fizz buzz with list comprehension with comment line added

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,4 @@
+# 1st if check if the number beetwen range of 100 is divisible by both 3 and 5 then print fizzbuzz, if no. is divisible by 3 print fizz else if it is divible by 5 print buzz
 output = [("fizzbuzz" if(i % 3 == 0 and i % 5 == 0) else "buzz" if i % 5 == 0 else "fizz" if i % 3 == 0  else i) for i in
          range(1, 100)]
 print(output)
